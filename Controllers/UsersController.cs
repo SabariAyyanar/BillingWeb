7c0febc9395bca4af1f78a73f9a0983840ec4598
gwalvanshi <gwalvanshi@gmail.com>
2018-07-14 18:53:56 +0530

    Login Page implemented

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BillingWeb;
+
+namespace BillingWeb.Controllers
+{
+    public class UsersController : Controller
+    {
+        private Billing4Entities db = new Billing4Entities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var tblUsers = db.tblUsers.Include(t => t.tblRole);
+            return View(tblUsers.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoleId = new SelectList(db.tblRoles, "RoleId", "Role");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,UserName,Password,CreatedDate,IsActive,RoleId,AddedBy")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblUsers.Add(tblUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleId = new SelectList(db.tblRoles, "RoleId", "Role", tblUser.RoleId);
+            return View(tblUser);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleId = new SelectList(db.tblRoles, "RoleId", "Role", tblUser.RoleId);
+            return View(tblUser);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,UserName,Password,CreatedDate,IsActive,RoleId,AddedBy")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleId = new SelectList(db.tblRoles, "RoleId", "Role", tblUser.RoleId);
+            return View(tblUser);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblUser tblUser = db.tblUsers.Find(id);
+            db.tblUsers.Remove(tblUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
